6f08d0a78a7ef5fc4da03b0cc193e8133863d091
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-04-20 03:31:08 +0530

    task tracker cli ready

---
+++ task-tracker/src/types.ts
@@ -0,0 +1,9 @@
+export type TaskStatus = 'todo' | 'in-progress' | 'done';
+
+export interface Task{
+    id: number;
+    description: string;
+    status: TaskStatus;
+    createdAt: string;
+    updatedAt: string;
+}
