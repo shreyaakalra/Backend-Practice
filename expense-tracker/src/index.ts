fd8982710df920dbe521a339165b720775d7376f
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-04-24 02:34:57 +0530

    expense logic

---
+++ expense-tracker/src/index.ts
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+const command = args[0];
+
+switch(command){
+    case "add":
+        const n = args.length;
+        let description,amount;
+        for(let i=0; i<n; i++){
+            if(args[i]==="--description"){
+                description = args[i+1];
+            }
+            if(args[i]==="--amount"){
+                amount = Number(args[i+1]);
+            }
+        }
+
+        if(!description || amount===undefined || amount<0){
+            console.log("Add description and amount!");
+        }
+
+        addExpense(description, amount);
+        break;
+}
