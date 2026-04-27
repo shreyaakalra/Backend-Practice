fb8ca714b3bff23b12b5f87135a566084bf4fe03
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-04-27 23:01:46 +0530

    completed the summary function

--- expense-tracker/src/expenseLogic.ts
+++ expense-tracker/src/expenseLogic.ts
@@ -7,7 +7,7 @@ export const addExpense = async (description: string, amount: number) => {
 
     const newID = lastExpense ? lastExpense.id + 1 : 1;
 
-    const now = new Date().toISOString();
+    const now = new Date().toLocaleDateString('en-GB');
 
     const newExpense = {
         id: newID,
@@ -24,4 +24,59 @@ export const addExpense = async (description: string, amount: number) => {
 
     console.log(`Expense added successfully (ID: ${newID})`);
 
+}
+
+export const listExpense = async() => {
+    const expenses = await getExpenses();
+
+    if(expenses.length === 0){
+        console.log("No expenses found!");
+        return;
+    }
+
+    console.table(expenses, ["id", "createdAt", "description", "amount"]);
+}
+
+export const deleteExpense = async(id: number) => {
+    const expenses = await getExpenses();
+
+    const updatedExpenses = expenses.filter(expense => expense.id!==id)
+
+    if(updatedExpenses.length === expenses.length){
+        console.log("expense not found!");
+        return;
+    }
+
+    await saveExpenses(updatedExpenses);
+    console.log("Expense deleted successfully!")
+}
+
+export const summaryExpense = async() => {
+    const expenses = await getExpenses();
+
+    let sum = 0;
+
+    for(let i=0; i<expenses.length; i++){
+        sum += expenses[i]?.amount || 0;
+    }
+
+    console.log(`Total expenses: $${sum}`)
+}
+
+export const summaryMonthExpense = async(month: number) => {
+    const expenses = await(getExpenses());
+    let amount = 0;
+
+    for(let i=0; i<expenses.length; i++){
+        const creationDate = expenses[i]?.createdAt;
+        const date = Number(creationDate?.split('/')[1]);
+
+        if(date===month){
+            amount += expenses[i]?.amount || 0;
+        }
+    }
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    console.log(`Total expenses for ${months[month-1]}: ${amount}`);
 }
--- expense-tracker/src/index.ts
+++ expense-tracker/src/index.ts
@@ -1,9 +1,10 @@
-import { addExpense } from "./expenseLogic";
+import { addExpense, deleteExpense, listExpense, summaryExpense, summaryMonthExpense } from "./expenseLogic";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 switch(command){
+
     case "add":
         const n = args.length;
         let description,amount;
@@ -23,4 +24,35 @@ switch(command){
 
         addExpense(description, amount);
         break;
+
+    case "list":
+        listExpense();
+        break;
+
+    case "delete":
+        const id = args[2];
+        if(!id){
+            console.log("Add id of the expense you wanna delete!")
+            break;
+        }
+
+        deleteExpense(Number(id));
+        break;
+
+    case "summary":
+        if(args[1]==="--month"){
+            const month = args[2];
+
+            if(!month){
+                console.log("add the month!");
+                break;
+            }
+
+            summaryMonthExpense(Number(month));
+            break;
+        }
+        else{
+            summaryExpense();
+            break;
+        }
 }
