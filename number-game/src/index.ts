67cfbe0590ea351470b4754be6b9c85ee21d4583
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-04-28 04:28:23 +0530

    refactored dry principle

--- number-game/src/index.ts
+++ number-game/src/index.ts
@@ -27,68 +27,25 @@ const numberGame = async() => {
             console.log("Let's start the game!");
             chances = 10;
             validLevel = true;
-
-            const targetNumber = Math.floor(Math.random() * 100) + 1;
-            let count = 0;
-
-            while(chances!==0){
-                const guess = await rl.question("Enter your guess: ");
-                count ++;
-                if(Number(guess) === targetNumber){
-                    console.log(`Congratulations! You guessed the correct number in ${count} attempts.`)
-                    rl.close();
-                    return;
-                }
-                else{
-                    if(Number(guess) > targetNumber){
-                        console.log(`Incorrect! The number is less than ${guess}.`)
-                    }
-                    else{
-                        console.log(`Incorrect! The number is greater than ${guess}.`)
-                    }
-                }
-                chances--;
-            }
-
-            console.log("All chances over, You lost.")
-
-            
         }
         else if(level==="2"){
             console.log(`Great! You have selected the Medium difficulty level.`);
             console.log("Let's start the game!");
             chances = 5;
             validLevel = true;
-
-            const targetNumber = Math.floor(Math.random() * 100) + 1;
-            let count = 0;
-
-            while(chances!==0){
-                const guess = await rl.question("Enter your guess: ");
-                count ++;
-                if(Number(guess) === targetNumber){
-                    console.log(`Congratulations! You guessed the correct number in ${count} attempts.`)
-                    rl.close();
-                    return;
-                }
-                else{
-                    if(Number(guess) > targetNumber){
-                        console.log(`Incorrect! The number is less than ${guess}.`)
-                    }
-                    else{
-                        console.log(`Incorrect! The number is greater than ${guess}.`)
-                    }
-                }
-                chances--;
-            }
-
-            console.log("All chances over, You lost.")
         }
         else if(level==="3"){
             console.log(`Great! You have selected the Hard difficulty level.`);
             console.log("Let's start the game!");
             chances = 3;
             validLevel = true;
+        }
+        else{
+            console.log("Please select a valid level : 1, 2 or 3.")
+        }
+
+    }
+
 
             const targetNumber = Math.floor(Math.random() * 100) + 1;
             let count = 0;
@@ -113,12 +70,6 @@ const numberGame = async() => {
             }
 
             console.log("All chances over, You lost.")
-        }
-        else{
-            console.log("Please select a valid level : 1, 2 or 3.")
-        }
-
-    }
 
 
     
