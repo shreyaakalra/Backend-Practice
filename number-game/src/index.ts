6ca71a66d1cd613a50faef43541729c730232741
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-04-27 23:36:37 +0530

    levels of the game

--- number-game/src/index.ts
+++ number-game/src/index.ts
@@ -9,5 +9,38 @@ const numberGame = async() => {
     console.log("I'm thinking of a number between 1 and 100.");
     console.log("You have 5 chances to guess the correct number.");
 
+    console.log("Please select the difficulty level:");
+    console.log("1. Easy (10 chances)")
+    console.log("2. Medium (5 chances)")
+    console.log("3. Hard (3 chances)")
+
     
+    let validLevel = false;
+
+    while(!validLevel){
+
+        const level = await rl.question("Enter your choice: ")
+
+        if(level==="1"){
+            console.log(`Great! You have selected the Easy difficulty level.`);
+            console.log("Let's start the game!");
+            validLevel = true;
+        }
+        else if(level==="2"){
+            console.log(`Great! You have selected the Medium difficulty level.`);
+            console.log("Let's start the game!");
+            validLevel = true;
+        }
+        else if(level==="3"){
+            console.log(`Great! You have selected the Hard difficulty level.`);
+            console.log("Let's start the game!");
+            validLevel = true;
+        }
+        else{
+            console.log("Please select a valid level : 1, 2 or 3.")
+        }
+
+    }
+    
+    rl.close();
 }
