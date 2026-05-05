aebfad5e7c779e1fd68dfa6dc5172eb5d1fd46c0
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-05 15:41:29 +0530

    dashboard admin

--- personal-blog/app/(admin)/dashboard/page.tsx
+++ personal-blog/app/(admin)/dashboard/page.tsx
@@ -1,1 +1,46 @@
 // Admin Home: Lists articles + edit/delete buttons
+
+import { getPosts } from "@/lib/posts";
+import Link from "next/link";
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return (
+    <div className="flex flex-col justify-center items-center p-10">
+        <div className="flex flex-row justify-between w-full max-w-2xl border-b pb-4 mb-10">
+            <div className="text-2xl font-bold py-2">Admin Dashboard</div>
+            <Link href="/add">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded font-bold ">
+                    + ADD NEW
+                </button>
+            </Link> 
+        </div>
+
+        <div>
+            <ul className="">
+                {posts.map((post) => {
+                    return(
+                        <li key={post.slug} className="">
+                            <div>
+                                <div>{post.title}</div>
+                                <div>{post.date}</div>
+                            </div>
+
+                            <div>
+                                <Link href={`/edit/${post.slug}`}>
+                                    <button>Edit</button>
+                                </Link>
+
+                                <button>Delete</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+// bg-blue-500 text-white px-4 py-2 rounded font-bold
--- personal-blog/app/(guest)/page.tsx
+++ personal-blog/app/(guest)/page.tsx
@@ -1,1 +1,26 @@
-// Home page : Lists all the articles
+// Home page : Lists all the articlesimport { getPosts } from "@/lib/posts";
+
+import { getPosts } from "@/lib/posts";
+import Link from "next/link";
+
+export default async function Home() {
+  const posts = await getPosts();
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-6xl mt-10">PERSONAL BLOG</div>
+      <div className="mt-20">
+        <ul>
+          {posts.map((post) => {
+            return(
+              <Link key={post.slug} href={`/articles/${post.slug}`}>
+                <li>
+                  {post.title} - {post.date}
+                </li>
+              </Link>  
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
