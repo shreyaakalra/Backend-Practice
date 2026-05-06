c4e7593ea9cf69fe6d45428cfb2e897672d97821
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-07 01:07:02 +0530

    have to expand pages

--- personal-blog/app/(guest)/page.tsx
+++ personal-blog/app/(guest)/page.tsx
@@ -7,7 +7,17 @@ export default async function Home() {
   const posts = await getPosts();
   return (
     <div className="flex flex-col justify-center items-center p-10">
-      <div className="flex flex-row justify-between w-full max-w-2xl border-b pb-4 mb-10 text-2xl font-bold py-2">PERSONAL BLOG</div>
+      <div className="flex flex-row justify-between w-full max-w-2xl border-b pb-4 mb-10">
+        <div className="text-2xl font-bold py-2">
+          PERSONAL BLOG
+        </div>
+        <Link href="/add">
+                <button className="bg-blue-500 text-white px-4 py-2 rounded font-bold ">
+                    Log In
+                </button>
+            </Link> 
+        </div>
+      
       <div className="w-full max-w-2xl">
         <ul className="flex flex-col gap-4">
           {posts.map((post) => {
