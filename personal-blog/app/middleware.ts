48e925675d3950bd2c867c97a7a7c7f5c8334ad3
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-08 14:25:27 +0530

    middleware.ts

--- personal-blog/app/middleware.ts
+++ personal-blog/app/middleware.ts
@@ -1,1 +1,24 @@
 // Acts like the bouncer - checks for cookies before allowing /dashboard access
+
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest){
+    // check if the users browser has the VIP stamp
+    const hasCookie = request.cookies.has('admin-session');
+
+    // if they dont have the stamp kick them to the login page
+    if(!hasCookie){
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // if they do have the stamp let them in
+    return NextResponse.next();
+}
+
+
+export const config = {
+    matcher: [
+        '/dashboard/:path*',
+        '/edit/:path*'
+    ]
+}
