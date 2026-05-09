644b2500ba193dff23e21e18f95aa2d91e251a81
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-10 00:19:24 +0530

    weather api

--- weather-api/server.js
+++ weather-api/server.js
@@ -10,10 +10,27 @@ app.use(express.json());
 
 app.get("/api/weather/:city", async (req, res) => {
     try{
-        
+        const city = req.params.city;
+
+        const geoRes = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.WEATHER_API_KEY}`);
+
+        const geoData = geoRes.data;
+
+        if(!geoData) throw new Error("city not found");
+
+        const {lat, lon} = geoData[0];
+
+        const weatherRes = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`);
+
+        res.json(weatherRes.data);
+
+        console.log("Fetching for city:", city);
+        console.log("Using API Key:", process.env.WEATHER_API_KEY ? "Key Found ✅" : "Key Missing ❌");
+
+    } catch (error) {
+        console.error("The API crashed", error.message);
     }
 })
-
 
 
 app.listen(PORT, () => {
