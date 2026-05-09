57ea028c683be3176d170d0739a9a20998301f55
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-09 23:57:29 +0530

    writing get route

--- weather-api/server.js
+++ weather-api/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const axios = require('axios');
 
 const express = require('express');
 
@@ -7,9 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({message: "The weather API engine is running!"});
+app.get("/api/weather/:city", async (req, res) => {
+    try{
+        
+    }
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`server is listening on the port: http://localhost:${PORT} `);
