b441997e709c483e8329b183a231e26f889a06b6
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-16 02:31:36 +0530

    user and to do scehma models

---
+++ todo-api/Todo.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose();
+
+const todoSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+},{
+    timestamps: true
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+export default Todo;
