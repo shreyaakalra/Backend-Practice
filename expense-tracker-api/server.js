20d3b3c44672d754ec8521d82b0651c2b36b3100
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-23 00:14:23 +0530

    sign up route

--- expense-tracker-api/server.js
+++ expense-tracker-api/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connectDB from "./db.js"
+import bcrypt from "bcrypt"
+
 const app = express();
 let PORT = 5001;
 
@@ -8,13 +10,50 @@ connectDB();
 app.get(express.json());
 
 // sign-up route
-app.post('/sign-up', (req, res) => {
+app.post('/sign-up', async(req, res) => {
     try{
-        const name = req.headers.name;
-        const email = req.headers.email;
-        const pass = req.headers.password;
-    } catch(err){
+        const { name, email, password } = req.body;
 
+        // now that we have the email first check if it already exists in the database
+
+        if(!name || !email || !password){
+            return res.status(403).json({
+                error: "ENTER YOUR NAME EMAIL AND PASS"
+            });
+        }
+
+        const reqEmail = User.findOne({email});
+
+        if(reqEmail){
+            return res.status(403).json({
+                error: "User Already Exists"
+            });
+        }
+
+        // should bcrypt the password first
+        const salt = bcrypt.genSalt(10);
+        const hashedPass = bcrypt.hash(password, salt);
+
+        const newUser = await new User({
+            name,
+            email,
+            password: hashedPass
+        });
+
+        await newUser.save();
+
+        const token = jwt.sign(
+            {id: newUser._id},
+            process.env.PRIVATE_KEY,
+            {expiresIn: "1h"}
+        )
+
+        return res.status(200).json({token})
+
+    } catch(err){
+        res.status(500).json({
+            error: "Something went wrong!"
+        });
     }
 });
 
