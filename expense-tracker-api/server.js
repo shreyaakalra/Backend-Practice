c936e55375e832323ade2b1a2da2269be75875aa
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-05-23 17:21:46 +0530

    login route

--- expense-tracker-api/server.js
+++ expense-tracker-api/server.js
@@ -1,46 +1,46 @@
 import express from "express";
 import connectDB from "./db.js"
 import bcrypt from "bcrypt"
+import "dotenv/config"
+import User from "./models/User.js"
+import jwt from "jsonwebtoken"
 
 const app = express();
 let PORT = 5001;
 
 connectDB();
 
-app.get(express.json());
+app.use(express.json());
 
 // sign-up route
-app.post('/sign-up', async(req, res) => {
+app.post('/sign-up', async (req, res)=>{
     try{
         const { name, email, password } = req.body;
 
-        // now that we have the email first check if it already exists in the database
-
         if(!name || !email || !password){
-            return res.status(403).json({
-                error: "ENTER YOUR NAME EMAIL AND PASS"
-            });
+            return res.status(404).json({
+                error: "bro add name email and password"
+            })
         }
 
-        const reqEmail = User.findOne({email});
+        const reqEmail = await User.findOne({email});
 
         if(reqEmail){
-            return res.status(403).json({
-                error: "User Already Exists"
+            return res.status(400).json({
+                error: "User already exists!"
             });
         }
 
-        // should bcrypt the password first
-        const salt = bcrypt.genSalt(10);
-        const hashedPass = bcrypt.hash(password, salt);
+        const salt  = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt);
 
-        const newUser = await new User({
+        const newUser = new User({
             name,
             email,
             password: hashedPass
         });
 
-        await newUser.save();
+       await newUser.save();
 
         const token = jwt.sign(
             {id: newUser._id},
@@ -48,28 +48,58 @@ app.post('/sign-up', async(req, res) => {
             {expiresIn: "1h"}
         )
 
-        return res.status(200).json({token})
+        res.status(200).json({token});
+
 
     } catch(err){
-        res.status(500).json({
-            error: "Something went wrong!"
-        });
+        return res.status(404).json({
+            error: `ERROR: ${err}`
+        })
     }
-});
+})
 
 // log-in route
-app.post('/login', (req, res) => {
+app.post('/login', async(req, res) => {
     try{
 
-    } catch(err){
+        // to login first we'd need to check if the email even esxists and for that we need a fucking email
+        const { email, password } = req.body;
 
+        const user = await User.findOne({email});
+
+        if(!user){
+            res.json({
+                error: "User doesn't exist. Sign up first."
+            })
+        }
+
+        const checkPass = await bcrypt.compare(password, user.password);
+
+        if(!checkPass){
+            return res.status(404).json({
+                error: "wrong password"
+            })
+        }
+
+        const token = jwt.sign(
+            {id: user._id},
+            process.env.PRIVATE_KEY,
+            {expiresIn: '1h'}
+        )
+
+        res.status(200).json({token});
+
+    } catch(err){
+        return res.status(404).json({
+            error: `ERROR: ${err}`
+        })
     }
 });
 
 // get expenses
 app.get('/expenses/:filter', (req, res) => {
     try{
-
+        
     } catch(err){
 
     }
